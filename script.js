32e1c5e62ae1a81ee7305e1bb7f5529ed86817b7
David <ddluwole@gmail.com>
2022-06-22 16:47:17 +0100

    Zuri first javascript commit that prints my name, height and country

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-console.log("My name is David ilori");
-console.log("I am 176cm tall");
-console.log("My country is Nigeria");
-
 alert("My name is David ilori");
 alert("I am 176cm tall");
 alert("My country is Nigeria");
