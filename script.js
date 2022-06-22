8ee485e7fa2a961b2400b1e914a99396e9c40046
David <ddluwole@gmail.com>
2022-06-22 15:30:26 +0100

    Last commit

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 console.log("My name is David ilori");
 console.log("I am 176cm tall");
 console.log("My country is Nigeria");
+
+alert("My name is David ilori");
+alert("I am 176cm tall");
+alert("My country is Nigeria");
